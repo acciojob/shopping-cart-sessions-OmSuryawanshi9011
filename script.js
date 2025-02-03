ec3046e7ed38995503e9feec96a454c7bf5599de
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-02-03 15:13:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
-
+// Cart data
+let cart = [];
+// Render product list
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
@@ -19,13 +21,24 @@ function renderProducts() {
     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
+
+  // Add event listeners to "Add to Cart" buttons
+  const addToCartButtons = document.getElementsByClassName("add-to-cart-btn");
+  for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", function() {
+      addToCart(this.dataset.id);
+    });
+  }
 }
 
 // Render cart list
 function renderCart() {}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	 const product = products.find((product) => product.id == productId);
+  cart.push(product);
+}
 
 // Remove item from cart
 function removeFromCart(productId) {}
